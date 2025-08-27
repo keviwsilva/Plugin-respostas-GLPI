402a9bc9394939ae7124158091e2a042c9de01f8
Kevin Willians <99736435+keviwsilva@users.noreply.github.com>
2025-08-27 09:42:36 -0300

    Update setup.php

--- setup.php
+++ setup.php
@@ -13,12 +13,16 @@ function plugin_version_customjs() {
     return [
         'name'           => "Custom JS",
         'version'        => '1.0.0',
-        'author'         => 'Kevin Willians',
+        'author'         => 'Seu Nome',
         'license'        => 'GPLv2+',
+        'homepage'       => 'https://glpi-project.org',
         'minGlpiVersion' => '10.0.0'
     ];
 }
 
+/**
+ * Verifica requisitos antes de instalar
+ */
 function plugin_customjs_check_prerequisites() {
     if (version_compare(GLPI_VERSION, '10.0.0', 'lt')) {
         echo "Este plugin requer GLPI 10 ou superior";
@@ -27,6 +31,23 @@ function plugin_customjs_check_prerequisites() {
     return true;
 }
 
+/**
+ * Verifica configuração
+ */
 function plugin_customjs_check_config($verbose = false) {
+    return true;
+}
+
+/**
+ * Função chamada ao instalar o plugin
+ */
+function plugin_customjs_install() {
+    return true;
+}
+
+/**
+ * Função chamada ao desinstalar o plugin
+ */
+function plugin_customjs_uninstall() {
     return true;
 }
