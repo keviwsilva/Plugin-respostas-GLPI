34c749b014256eab1032f0a337b08393deea57d7
Kevin Willians <99736435+keviwsilva@users.noreply.github.com>
2025-08-27 15:13:02 -0300

    Update alertas.js

--- js/alertas.js
+++ js/alertas.js
@@ -10,8 +10,8 @@ async function getTodosTecnicosTicket(ticketId) {
     try {
         const response = await fetch(`/apirest.php/Ticket/${ticketId}/Ticket_User/`, {
             headers: {
-                'Session-Token': 'USER_TOKEN',
-                'App-Token': 'APP_TOKEN'
+                'Session-Token': USER_TOKEN,
+                'App-Token': APP_TOKEN,
             }
         });
         
@@ -162,6 +162,7 @@ pegarUserIdDoBackend().then(userId => {
     });
 
 })();
+
 
 
 
