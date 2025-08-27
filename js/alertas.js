1b05d3d5665f22f528ca2a384f290a438d0b4ff1
Kevin Willians <99736435+keviwsilva@users.noreply.github.com>
2025-08-27 16:39:03 -0300

    Update alertas.js

--- js/alertas.js
+++ js/alertas.js
@@ -44,7 +44,7 @@ async function getTodosTecnicosTicket(ticketId) {
         const data = await response.json();
         console.log(data)
         // Filtrar apenas técnicos (type 1 e 4)
-        const tecnicos = data.filter(item => item.type === 1 || item.type === 4);
+        const tecnicos = data.filter(item => item.type === 2 || item.type === 4);
         
         console.log('Todos os técnicos do ticket:', tecnicos);
         return tecnicos;
@@ -141,7 +141,7 @@ async function getTodosTecnicosTicket(ticketId) {
         console.log(tickets)
         tickets.forEach(ticket => {
             if (!ticket.id || !ticket.date_mod) return;
-             const tecnicos = getTodosTecnicosTicket(ticket.users_id);
+             const tecnicos = getTodosTecnicosTicket(ticket.id);
             console.log(tecnicos)
             // Mostra só para tickets atribuídos ao usuário do cookie
             if (ticket.users_id_assign !== MEU_USER_ID) return;
@@ -167,6 +167,7 @@ async function getTodosTecnicosTicket(ticketId) {
     });
 
 })();
+
 
 
 
